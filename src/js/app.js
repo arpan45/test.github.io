36c59325c258766b0d234ca98c4a34bb75e63f37
Arpan Adhikari <arpan@uddipans-MacBook-Air.local>
2019-11-25 00:38:25 +0530

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", function () {
         }
       },
       "color": {
-        "value": "#ffffff"
+        "value": "#5c8df4"
       },
       "shape": {
         "type": "polygon",
